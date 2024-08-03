c076df3a7396dbc2de45d5fe4b49b2ea0c39e1e9
JamesAChavesForero <javeschames2024@gmail.com>
2024-08-03 17:43:52 -0500

    .

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,9 @@ var rateOptions = document.querySelectorAll(".smileRating img");
 
 rateOptions.forEach((rate) => {
   rate.addEventListener("click", () => {
-    rateOptions.forEach(rate => rate.src = `./assets/imgs/${rate.name}.svg`)
-    rate.src = `./assets/imgs/${rate.name}Selected.svg`;
+    console.log("new bug")
+    rateOptions.forEach(rate => rate.src = `/assets/imgs/${rate.name}.svg`)
+    rate.src = `/assets/imgs/${rate.name}Selected.svg`;
     document.getElementById("moodRate").value = rate.name
     console.log(document.getElementById("moodRate").value, " = ", rate.name)
   });
