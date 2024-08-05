83d7f0fd7289af378e8c4b6133c2e7aa322267e7
JamesAChavesForero <javeschames2024@gmail.com>
2024-08-05 16:28:56 -0500

    finished

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,17 @@ var call_form_ = document.querySelector("#formContainer form");
 var call_formData = new FormData(call_form_);
 var call_params = "";
 var rate = document.querySelector(".smileRating");
+var progressNum = "%";
 var rateOptions = document.querySelectorAll(".smileRating img");
 
 rateOptions.forEach((rate) => {
   rate.addEventListener("click", () => {
-    rateOptions.forEach(rate => rate.src = `./assets/imgs/${rate.name}.svg`)
+    rateOptions.forEach(
+      (rate) => (rate.src = `./assets/imgs/${rate.name}.svg`)
+    );
     rate.src = `./assets/imgs/${rate.name}Selected.svg`;
-    document.getElementById("moodRate").value = rate.name
-    console.log(document.getElementById("moodRate").value, " = ", rate.name)
+    document.getElementById("moodRate").value = rate.name;
+    console.log(document.getElementById("moodRate").value, " = ", rate.name);
   });
 });
 
@@ -27,25 +30,23 @@ let call_trigger = async (url, data) => {
   return response; // parses JSON response into native JavaScript objects
 };
 
-
-
 call_form_.addEventListener("submit", (e) => {
   e.preventDefault();
-  
-document.querySelectorAll("input[type=checkbox]").forEach(checkBox => {
-  console.log(checkBox, checkBox.value, checkBox.name)
-  checkBox.checked && call_formData.append(`${checkBox.name}`, `${checkBox.value}`)
-})
-document.querySelectorAll("input[type=radio]").forEach(radioBtn => {
-  radioBtn.checked && call_formData.append(`${radioBtn.name}`, `${radioBtn.value}`)
-})
-  console.log(call_formData,call_formData.entries())
+
+  document.querySelectorAll("input[type=checkbox]").forEach((checkBox) => {
+    console.log(checkBox, checkBox.value, checkBox.name);
+    checkBox.checked &&
+      call_formData.append(`${checkBox.name}`, `${checkBox.value}`);
+  });
+
   for (var [key, value] of call_formData.entries()) {
+    console.log("key :", key, key === "progress");
     call_params += `${key}=${
-    
-      document.querySelector("*[name=" + key + "] ").value //  finds first radius and always sends 20%
+      key === "progress"
+        ? progressNum
+        : document.querySelector("*[name=" + key + "]").value //  finds first radius and always sends 20%
     }&`;
-    console.log(key, value)
+    console.log(key, value);
   }
 
   // dataObject = Object.fromEntries(fn)
@@ -56,8 +57,38 @@ document.querySelectorAll("input[type=radio]").forEach(radioBtn => {
   ).then((data) => {
     console.log(data);
     call_form_.reset();
-    window.alert("Your Daily Report Has Been Sent! thank you!")
+    window.alert("Your Daily Report Has Been Sent! thank you!");
     // window.location.reload()
   });
+});
 
+document.querySelectorAll("input[type=radio]").forEach((radioBtn) => {
+  radioBtn.addEventListener("click",({target})=>{
+    call_formData.set(`${target.name}`, `${target.value}`)
+    progressNum = call_formData.get(`${target.name}`)
+  })
+});
+
+const uploadButton = document.getElementById("file");
+
+uploadButton.addEventListener("change", async function () {
+  const fileInput = document.getElementById("file");
+
+  const file = fileInput.files[0];
+
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("validation", ["jpg", "png", "pdf"]);
+
+  const request = await fetch("https://api.cevimedone.com/technician-form/", {
+    method: "POST",
+    body: formData,
+  });
+
+  const response = await request.json();
+
+  if (response.status === "success") {
+    document.querySelector("#fileURL").value =
+      "https://api.cevimedone.com/technician-form/" + response.url;
+  } else alert(response.message);
 });
